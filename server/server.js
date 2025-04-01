feb5fe4aa3e819cd76adf4061d8fa350d1f3672b
Balram Dhakad <bdhakad886@gmail.com>
2025-04-01 14:52:56 +0530

    Render fix

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ import cors from "cors";
 import userRoutes from "./routes/Routes.js";
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 const app = express();
 
 // Define dbHOST before using it
 const dbHOST = process.env.MONGO_DB_URL;
+
+const __dirname = path.resolve();
 
 // Improved MongoDB connection with error handling
 mongoose
@@ -37,10 +40,10 @@ app.use(cors({
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "15mb" }));
 
-// Basic route
-app.get('/', (req, res) => {
-    res.send('Products API running');
-});
+// // Basic route
+// app.get('/', (req, res) => {
+//     res.send('Products API running');
+// });
 
 // API routes
 app.use("/api", userRoutes);
@@ -49,6 +52,12 @@ app.use("/api", userRoutes);
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
+});
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 });
 
 // Server configuration for Vercel
